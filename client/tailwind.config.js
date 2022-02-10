f5ad9124892592ac6fafb5f8e508bf7cbfce4e32
Jimmy Wu <jimmywu0605@gmail.com>
2022-02-10 12:26:23 -0500

    init frontend with cra, typescript, and tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      green: {
+        light: '#c7ee63',
+        DEFAULT: '#abd155',
+        dark: '#96bc32'
+      },
+      blue: {
+        light: '#546fb5',
+        DEFAULT: '#5e6b8d',
+        dark: '#2c2e50'
+      }
+    },
+    fontFamily: {
+      bunuelo: "BunueloLight",
+      bunueloBold: "BunueloBold",
+      asap: "Asap"
+    }
+  },
+  plugins: [],
+}
