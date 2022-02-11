15c10db89fc876b8b9c3885f1cd63a863d9749df
Jimmy Wu <jimmywu0605@gmail.com>
2022-02-11 16:33:08 -0500

    add license headers

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Combomark } from "assets/openliberty_combomark.svg";
