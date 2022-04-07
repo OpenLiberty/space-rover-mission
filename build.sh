264c95a22a47f816477fb49a7e17018b528ae8b4
Jimmy Wu <jimmywu0605@gmail.com>
2022-04-07 16:43:26 -0400

    remove unneeded scripts

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd server
-./mvnw install -DskipTests=true
-cd ../leaderboard
-./mvnw install 
-cd ..
-docker-compose down
-docker-compose up --build
-
--- services/leaderboard/startLeaderboard.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./mvnw install
-#docker-compose up --build
-x#docker-compose up > leaderboard.log 2>&1 &
-exit
