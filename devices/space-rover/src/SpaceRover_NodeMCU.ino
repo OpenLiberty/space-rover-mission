a211e7c7a86f4fc20dc6415037144f3931dc56b7
Prashanth G <pgunapal@ca.ibm.com>
2022-06-09 10:42:19 -0400

    Update SpaceRover_NodeMCU.ino

--- devices/space-rover/src/SpaceRover_NodeMCU.ino
+++ devices/space-rover/src/SpaceRover_NodeMCU.ino
@@ -31,7 +31,7 @@ WebSocketsServer webSocket = WebSocketsServer(5045);
   VARIABLES
 ************/
 char ssid[] = "OL_DEMO";  // Dedicated WiFi local network for demo.
-char pass[] = "was4ever";
+char pass[] = "######"; // UPDATE WI-FI PASSWORD
 
 // Serial Communication between Arduino and NodeMCU
 const byte numChars = 32;
